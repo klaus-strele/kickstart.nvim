f80449f55fe4aa7b9d52a16db4dfc56a1fa20e2b
Klaus Strele <klaus.strele@strele.com>
2026-09-22 22:32:57 +0200

    added json to treesitter

--- init.lua
+++ init.lua
@@ -962,7 +962,7 @@ do
   vim.pack.add { { src = gh 'nvim-treesitter/nvim-treesitter', version = 'main' } }
 
   -- Ensure basic parsers are installed
-  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python', 'javascript' }
+  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python', 'javascript', 'json' }
   require('nvim-treesitter').install(parsers)
 
   ---@param buf integer
